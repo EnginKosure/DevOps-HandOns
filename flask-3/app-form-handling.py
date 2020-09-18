55a12264809d840eb3bd02f74f0c8489aa7afa3c
EnginKosure <engin.kosure@cronos.be>
2020-09-18 23:39:57 +0200

    flask app  started

--- flask-3/app-form-handling.py
+++ flask-3/app-form-handling.py
@@ -1,7 +1,28 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
+@app.route('/')
 def home():
-    return render_template('index.html')
+    return render_template('index.html', name='EnKo')
+
+
+@app.route('/greet', methods=['GET'])
+def greet():
+    if 'user' in request.args:
+        usr = request.args['user']
+        return render_template('greet.html', user=usr)
+    else:
+        render_template(
+            'greet.html', user='Send your user name with user in query string')
+        # http://127.0.0.1:5000/greet?user=Ahmet seklinde ? arkasindan key value pair gonderebiliyoruz.
+        # ikinci birtane gondereceksek araya & koyup yeni key value yu &key=value seklinde yaziyoruz
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+    # Bu app e heryerden ulasilmasini istiyorsak host a everywhere karsiligi olan 0.0.0.0,
+    #  porta http karsiligi olan 80 atanir
+    # app.run(host='0.0.0.0', port=80)
